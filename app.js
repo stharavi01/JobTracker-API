ed9c8a33e41a0d0fa8f4c6894e7411f248c65653
stharavi01 <sanayashe@gmail.com>
2024-04-11 23:08:58 +0545

    fix cyclic deploy issue

--- app.js
+++ app.js
@@ -37,12 +37,12 @@ app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(process.env.PORT || 3000, () =>
+    app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
